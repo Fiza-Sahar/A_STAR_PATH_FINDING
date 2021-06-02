05936c053712f90c0466a5da5e8d79be390cd289
Fiza-Sahar <fizasahar66@gmail.com>
2021-06-03 03:14:08 +0500

    1st commit

---
+++ Install_Requirements.py
@@ -0,0 +1,18 @@
+import subprocess
+import sys
+import get_pip
+import os
+import importlib
+import contextlib
+
+def install(package):
+    '''
+    installs a package using pip
+
+    :param package: string
+    '''
+    subprocess.call([sys.executable, "-m", "pip", "install", package])
+
+
+required = []
+failed = []
